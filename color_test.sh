171078c436c24fc9d2c2bbe9af8a73fc543627eb
user <certemail@hotmail.com>
2017-09-06 18:37:41 -0400

    updated

--- color_test.sh
+++ color_test.sh
@@ -1,5 +1,8 @@
 
-for i in {1..255}; do 
+for i in {0..255}; do 
     color=$(tput setaf ${i})
-    printf "${color}${i}\n" 
+    printf "${color} ${i}" 
+    if ! ((i % 15)); then echo -e "\n"; fi 
 done
+
+echo -e "\n"
